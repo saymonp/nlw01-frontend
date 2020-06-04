e45a05af6acc29bb4fb679691beb4dff76bbb609
saymont <saymonp.trevisan@gmail.com>
2020-06-04 16:05:48 -0300

    Selecionar municipio

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowDownLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker } from "react-leaflet";
@@ -21,10 +21,17 @@ interface IBGEUFResponse {
     sigla: string;
 }
 
+interface IBGECityResponse {
+    nome: string;
+}
+
 const CreatePoint = () => {
 
     const [items, setItems] = useState<Item[]>([]);
     const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+
+    const [selectedUf, setSelectedUf] = useState("0");
 
     useEffect(() => {
         api.get("items").then(response => {
@@ -34,121 +41,148 @@ const CreatePoint = () => {
 
     useEffect(() => {
         axios.get<IBGEUFResponse[]>("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(response => {
-        const ufInitials = response.data.map(uf => uf.sigla);
+            const ufInitials = response.data.map(uf => uf.sigla);
 
-        setUfs(ufInitials);
+            setUfs(ufInitials);
         })
-    }, [])
+    }, []);
 
-    return (
-        <div id="page-create-point">
-            <header>
-                <img src={Logo} alt="Ecoleta" />
+    useEffect(() => {
+        if (selectedUf === "0") {
+            return;
+        }
+    }, [selectedUf]);
 
-                <Link to="/">
-                    <FiArrowDownLeft />
-                    Voltar para Home
+    axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+        const cityNames = response.data.map(city => city.nome);
+
+        setCities(cityNames);
+    })
+
+
+function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+    const uf = event.target.value;
+    setSelectedUf(uf);
+}
+
+
+
+return (
+    <div id="page-create-point">
+        <header>
+            <img src={Logo} alt="Ecoleta" />
+
+            <Link to="/">
+                <FiArrowDownLeft />
+                Voltar para Home
                 </Link>
-            </header>
+        </header>
 
-            <form>
-                <h1>Cadastro do <br /> ponto de coleta</h1>
-                <fieldset>
-                    <legend>
-                        <h2>Dados</h2>
-                    </legend>
+        <form>
+            <h1>Cadastro do <br /> ponto de coleta</h1>
+            <fieldset>
+                <legend>
+                    <h2>Dados</h2>
+                </legend>
 
+                <div className="field">
+                    <label htmlFor="name">Nome da Entidade</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                    />
+                </div>
+
+                <div className="field-group">
                     <div className="field">
-                        <label htmlFor="name">Nome da Entidade</label>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                        />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="whatsapp">Whatsapp</label>
                         <input
                             type="text"
-                            name="name"
-                            id="name"
+                            name="whatsapp"
+                            id="whatsapp"
                         />
                     </div>
-
-                    <div className="field-group">
-                        <div className="field">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                name="email"
-                                id="email"
-                            />
-                        </div>
-                        <div className="field">
-                            <label htmlFor="whatsapp">Whatsapp</label>
-                            <input
-                                type="text"
-                                name="whatsapp"
-                                id="whatsapp"
-                            />
-                        </div>
-                    </div>
-                </fieldset>
+                </div>
+            </fieldset>
 
 
-                <fieldset>
-                    <legend>
-                        <h2>Endereço</h2>
-                        <span>Selecione o endereço no mapa</span>
-                    </legend>
+            <fieldset>
+                <legend>
+                    <h2>Endereço</h2>
+                    <span>Selecione o endereço no mapa</span>
+                </legend>
 
-                    <Map center={[49.2885584, -123.1126744]} zoom={17}>
-                        <TileLayer
-                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Marker position={[49.2885584, -123.1126744]} />
-                    </Map>
+                <Map center={[49.2885584, -123.1126744]} zoom={17}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[49.2885584, -123.1126744]} />
+                </Map>
 
 
-                    <div className="field-group">
-                        <div className="field">
-                            <label htmlFor="uf">
-                                Estado (UF)
+                <div className="field-group">
+                    <div className="field">
+                        <label htmlFor="uf">
+                            Estado (UF)
                             </label>
-                            <select name="uf" id="uf">
-                                <option value="0">Selecione uma UF</option>
-                                {ufs.map(uf => (
-                                    <option key={uf} value={uf}>{uf}</option>
-                                ))}
-                            </select>
-                        </div>
-                        <div className="field">
-                            <label htmlFor="city">
-                                Cidade
-                            </label>
-                            <select name="city" id="city">
-                                <option value="0">Selecione uma cidade</option>
-                            </select>
-                        </div>
+                        <select
+                            name="uf"
+                            id="uf"
+                            value={selectedUf}
+                            onChange={handleSelectUf}>
+                            <option value="0">Selecione uma UF</option>
+                            {ufs.map(uf => (
+                                <option key={uf} value={uf}>{uf}</option>
+                            ))}
+                        </select>
                     </div>
-                </fieldset>
+                    <div className="field">
+                        <label htmlFor="city">
+                            Cidade
+                            </label>
+                        <select name="city" id="city">
+                            <option value="0">Selecione uma cidade</option>
+                            {cities.map(city => (
+                                <option key={city} value={city}>{city}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+            </fieldset>
 
-                <fieldset>
-                    <legend>
-                        <h2>Items de coleta</h2>
-                        <span>Selecione um ou mais items abaixo</span>
-                    </legend>
-                    <ul className="items-grid">
-                        {
-                            items.map(item => (
-                                <li key={item.id}>
-                                    <img src={item.image_url} alt={item.title} />
-                                    <span>{item.title}</span>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </fieldset>
-                <button type="submit">Cadastrar ponto de coleta</button>
-            </form>
+            <fieldset>
+                <legend>
+                    <h2>Items de coleta</h2>
+                    <span>Selecione um ou mais items abaixo</span>
+                </legend>
+                <ul className="items-grid">
+                    {
+                        items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </fieldset>
+            <button type="submit">Cadastrar ponto de coleta</button>
+        </form>
 
 
 
-        </div>
-    );
+    </div>
+);
 }
 
 export default CreatePoint;
